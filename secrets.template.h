c52c68b02630a474abcaf585dfae0f090be1c3d8
Alex Chang <alexchang42@outlook.com>
2026-03-24 12:49:46 -0400

    Update template comments for better clarity

--- secrets.template.h
+++ secrets.template.h
@@ -1,5 +1,5 @@
 // secrets.template.h - TEMPLATE FILE
-// INSTRUCTIONS: Copy this file, rename it to "secrets.h", and fill in your credentials.
+// INSTRUCTIONS: Copy this file, rename it to "secrets.h", and fill in with the proper credentials.
 // DO NOT upload your renamed "secrets.h" to a public repository.
 
 #ifndef SECRETS_H
@@ -8,8 +8,8 @@
 	#define SECRET_SSID "YOUR_WIFI_NAME_HERE"
 	#define SECRET_PASS "YOUR_WIFI_PASSWORD_HERE"
 
-	// --- Blynk Cloud Credentials ---
-	// These can be found in the Blynk.Console under "Device Info"
+	// --- Blynk Credentials ---
+	// These can be found in the Blynk.Console template and device overview/information pages
 	#define SECRET_BLYNK_TEMPLATE_ID   "TMPLXXXXXXX"
 	#define SECRET_BLYNK_TEMPLATE_NAME "Your Project Name"
 	#define SECRET_BLYNK_AUTH_TOKEN    "Your_Auth_Token_Here"
